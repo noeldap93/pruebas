5a283a9e226ffe48f11055a678ed5a3402de1284
noeldap93 <aragonknoel@gmail.com>
2014-06-10 21:57:01 -0500

    lnoti

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         window.plugin.backgroundMode.enable();
+        window.plugin.notification.local.add({ message: 'Great app!' });
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
